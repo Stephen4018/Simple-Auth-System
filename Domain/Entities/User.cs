b2c0d89945e014a151cd0883b58ced0ba2ff29d6
stephen4018 <stephenohaeri4018@gmail.com>
2025-05-10 19:10:14 +0100

    Setting up interfaces

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
+    }
+}
---
+++ Domain/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(User user);
+        bool ValidateToken(string token);
+        string GetUserIdFromToken(string token);
+    }
+}
