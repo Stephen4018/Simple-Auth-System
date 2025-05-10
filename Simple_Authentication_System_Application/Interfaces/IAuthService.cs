a20dfa7b045181c2ae00db6f4b47ef123c849e4c
stephen4018 <stephenohaeri4018@gmail.com>
2025-05-10 23:27:52 +0100

    service set up

---
+++ Simple_Authentication_System_Application/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Simple_Authentication_System_Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Authentication_System_Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterUserAsync(RegisterUserDto registerDto);
+        Task<AuthResponseDto> LoginAsync(LoginUserDto loginDto);
+        Task<UserDto> GetUserByIdAsync(Guid userId);
+    }
+}
---
+++ Simple_Authentication_System_Application/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Authentication_System_Application.Services
+{
+    public class AuthService
+    {
+        private readonly IUnitofWork _unitofWork;
+        private readonly IPasswordService _passwordService;
+        private readonly ITokenService _tokenService;
+        public AuthService(IUnitofWork unitofWork, IPasswordService passwordService, ITokenService tokenService)
+        {
+            _unitofWork = unitofWork;
+            _passwordService = passwordService;
+            _tokenService = tokenService;
+        }
+
+
+    }
+}
