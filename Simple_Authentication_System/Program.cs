71fbe991c46e976272dc1a92b8bb839f72c7cac5
stephen4018 <stephenohaeri4018@gmail.com>
2025-05-11 23:06:59 +0100

    external service registering

--- Simple_Authentication_System/Program.cs
+++ Simple_Authentication_System/Program.cs
@@ -1,16 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using Simple_Authentication_System_Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddServices(builder.Configuration);
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerServices();
+
+int isSwaggerEnabled = builder.Configuration.GetValue<int>("PUBLISH_API_SWAGGER");
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (isSwaggerEnabled == 1)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -18,6 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
